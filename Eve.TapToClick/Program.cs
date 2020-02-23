4eec68971fe85cffd0a7332837fe667b7693547b
Kevin Fisher <megabytefisher@gmail.com>
2020-02-23 12:54:46 -0600

    Add --kill-instance argument for use during uninstall; bump version to 1.0.5.0

--- Eve.TapToClick/Program.cs
+++ Eve.TapToClick/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Eve.TapToClick.Forms;
 using Eve.TapToClick.Utilities;
 using System;
-using System.Linq;
+using System.Diagnostics;
 using System.Threading;
 using System.Windows.Forms;
 
@@ -15,13 +15,41 @@ namespace Eve.TapToClick
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            if (Environment.GetCommandLineArgs().Any(s => s.ToLower() == "--remove-startup-task"))
+            bool removeStartupTask = false;
+            bool killInstance = false;
+            bool restartInstance = false;
+
+            foreach (string arg in args)
             {
-                AutoRun.RemoveStartupTask();
-                return;
+                if (arg.ToLower() == "--remove-startup-task")
+                    removeStartupTask = true;
+                else if (arg.ToLower() == "--kill-instance")
+                    killInstance = true;
             }
+
+            if (removeStartupTask)
+                AutoRun.RemoveStartupTask();
+
+            if (killInstance || restartInstance)
+            {
+                Process currentProcess = Process.GetCurrentProcess();
+
+                Process[] otherProcesses = Process.GetProcessesByName(currentProcess.ProcessName);
+                foreach (Process otherProcess in otherProcesses)
+                {
+                    if (otherProcess.Id != currentProcess.Id)
+                    {
+                        otherProcess.Kill();
+                    }
+                }
+
+                Thread.Sleep(100);
+            }
+
+            if (removeStartupTask || killInstance)
+                return;
 
             using (Mutex mutex = new Mutex(false, $"Global\\{instanceGuid}"))
             {
